5d27604cb2a6f96b4134d938aa1efc1ebc22d2eb
Diego262001 <dhdiego262001@outlook.com>
2023-11-02 22:19:01 -0600

    Actividad 1

---
+++ Actividad-1-js/app.js
@@ -0,0 +1,34 @@
+// 1
+
+let nomUs = prompt("Ingresa un número");
+
+let op = (nomUs * 2);
+console.log(op);
+
+
+// 2
+
+let num1 = prompt("Ingresa un número");
+let num2 = prompt("Ingresa otro número");
+
+let rest = (Number(num1) - Number(num2));
+console.log(rest);
+
+
+// 3
+
+let r = 5.3;
+let pi = 3.1416;
+
+let areaCirculo = (pi*r**2);
+console.log(areaCirculo);
+
+
+// 4
+
+const p = 5;
+const D = 8;
+const d = 6;
+
+let areaRombo = (D*d/2);
+console.log(areaRombo);
