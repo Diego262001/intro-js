8a15a84a45037cf3cdf4b310158d35ec10ee7f73
Diego262001 <dhdiego262001@outlook.com>
2023-11-14 19:50:19 -0600

    feat: Se agrega ejercicio 1-Condicionales

---
+++ Ejercicio-1-Condicionales/app.js
@@ -0,0 +1,75 @@
+// Ejercicio 1
+
+/*let pregunta = prompt("¿Eres bellisimo/a?").toLowerCase();
+
+if(pregunta == "si") {
+    console.log("Ciertamente lo eres");
+} else {
+    console.log("No te creo");
+}   */
+
+
+// Ejercicio 2
+
+/*let num = Number(prompt("Ingresa un número"));
+
+if(num % 2 == 0) {
+    console.log(`El número ${num} es divisible entre 2`)
+} else {
+    console.log(`El número ${num} no es divisible entre 2`)
+}   */
+
+
+// Ejercicio 3
+
+/*let numUsuario = Number(prompt("Ingresa un número par"));
+
+if(numUsuario % 2 == 0) {
+    console.log(`El número ${numUsuario} es par`);
+} else {
+    console.log(`El número ${numUsuario} es impar`);
+}   */
+
+
+// Ejercicio 4
+
+/*let numCliente = Number(prompt("Ingrese un número de cliente"));
+
+if(numCliente == 1000) {
+    console.log(`Felicidades!!!, Ganaste un premio`);
+} else {
+    console.log(`Lo sentimos, tu numero de cliente es el: ${numCliente}, sigue participando`);
+}    */
+
+
+// Ejercicio 5
+
+/*let num1 = Number(prompt("Ingresa un número"));
+let num2 = Number(prompt("Ingresa otro número"));
+
+if(num1 < num2) {
+    console.log(`El número ${num1} es menor que el número ${num2}`);
+} else if (num1 < num2) {
+    console.log(`El número ${num2} es menor que el número ${num1}`)
+}  */
+
+
+// Ejercicio 7
+
+/*let dia = prompt("Ingresa un dia de la semana");
+
+switch(dia) {
+    case "Lunes":
+        alert("Buen inicio de semana");
+        break;
+    case "Viernes":
+        alert("Vamos a bebeeeeer");
+        break;
+    case "Sabado":
+        alert("Descansa")
+        break;
+    default:
+        alert("Ya casi es fin de semana");
+}   */
+
+
